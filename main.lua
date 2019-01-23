bf65d543da5dd63cb2e3eadf2e0cfcea6542c2b3
jamescaron37 <jamescaron37@yahoo.fr>
2019-01-23 22:12:24 +0100

    Ajout ListePointsGui

--- main.lua
+++ main.lua
@@ -9,9 +9,12 @@ love.graphics.setDefaultFilter("nearest")
 if arg[#arg] == "-debug" then require("mobdebug").start() end
 
 local ListePoints = {}
+local ListePointsGui = {}
 local Point = {}
 Point.x = 0
 Point.y = 0
+Point.name = "P"
+Point.position = 0
 
 function DrawPoint()
   
@@ -46,12 +49,24 @@ function love.draw()
   for N=1,#ListePoints do
   
     love.graphics.circle("fill",ListePoints[N].x,ListePoints[N].y,2)
-  
+    ListePoints[N].position = N
   end
  
   
 love.graphics.setColor(255, 255, 255)
 love.graphics.print("Nombre de point : "..tostring(#ListePoints),(LARGEUR-(LARGEUR/4))+5,0)
+love.graphics.line((LARGEUR-(LARGEUR/4)), 20, LARGEUR, 20)
+
+  if #ListePoints ~= 0 then
+    local yGui = 30
+    for N =1,#ListePoints do
+      ListePointsGui[N] = ListePoints[N]
+      love.graphics.print("P"..tostring(ListePointsGui[N].position),(LARGEUR-(LARGEUR/4))+10,yGui)
+      love.graphics.print("x:"..tostring(ListePointsGui[N].x),(LARGEUR-(LARGEUR/4))+60,yGui)
+      love.graphics.print("y:"..tostring(ListePointsGui[N].y),(LARGEUR-(LARGEUR/4))+110,yGui)
+      yGui = yGui + 15
+    end
+  end
 
 end
 
@@ -59,9 +74,16 @@ function love.mousepressed(x, y, button, istouch)
    if button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
      
      local point = {}
-     point.x = x
-     point.y = y
-      
+     
+     if x > (LARGEUR-(LARGEUR/4)) then
+       
+      point.x = (LARGEUR-(LARGEUR/4))-4
+      point.y = y
+      else
+    
+      point.x = x
+      point.y = y
+      end
       table.insert(ListePoints,point)
    end
 end
